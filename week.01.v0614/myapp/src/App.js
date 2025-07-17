ae4a0a4c77980acc223984f8b9fc978dad5eedf7
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-18 08:53:53 +0900

    form textarea

--- week.01.v0614/myapp/src/App.js
+++ week.01.v0614/myapp/src/App.js
@@ -1,30 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
 import Parent from './component/Parent';
+import TextArea from './component/TextArea';
 
 const App = () => {
-  useEffect(() => {
-    const handleLoad = () => {
-      console.log("Window fully loaded!");
 
-      // 여기에 원하는 로직 작성
 
-    };
-
-    window.addEventListener("load", handleLoad);
-
-    // 컴포넌트 언마운트 시 이벤트 제거
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
-  }, []); // useEffect
 
   return (
     <div className="App">
       <header className="App-header">
         <h5>React, Hello World</h5>
-        <Parent />
+        <TextArea />
       </header>
     </div>
   );
