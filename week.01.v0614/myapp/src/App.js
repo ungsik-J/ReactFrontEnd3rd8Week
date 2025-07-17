47223e1dd067b94efc1593d2a2335e0b241344ad
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-17 17:43:35 +0900

    react_강의1

--- week.01.v0614/myapp/src/App.js
+++ week.01.v0614/myapp/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+import Edit from './component/Edit';
+
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>React</h2>
+
+        <Edit />
       </header>
     </div>
   );
---
+++ week.01.v0614/myapp/src/component/Edit.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Edit = () => {
+
+
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Edit;
