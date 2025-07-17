7002bac4d91ecfd881659c028097d079e9e01b22
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-18 08:24:00 +0900

    Parent, Child components

--- week.01.v0614/myapp/src/component/PushAndDelete.jsx
+++ week.01.v0614/myapp/src/component/PushAndDelete.jsx
@@ -2,58 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 
 const data = [];
 
-const PushAndDelete = () => {
-    const [customers, setCustomers] = useState(data);
-    const [name, setName] = useState('');
+const PushAndDelete = (props) => {
 
-    const handleOnClick = () => {
-        if (name) {
-            setCustomers(prevCustomers => [
-                ...prevCustomers,
-                { id: prevCustomers.length + 1, name }
-            ]);
+    const { name, setName, handleEnter, handleOnClick } = props;
 
-            setName('');
-        }
-    };
-
-    const handleDelete = (id) => {
-        setCustomers(prevCustomers => prevCustomers.filter(customer => customer.id !== id));
-    };
-
-    const handleEnter = (e) => {
-        if (e.key === 'Enter') {
-            handleOnClick();
-        }
-    };
-
-    const inputRef = useRef(null)
-    useEffect(() => {
-        inputRef.current?.focus();
-    }, []);
-
-    const vPlaceholder = "INPUT.placeholder"
 
     return (
-        <div>
-            <input
-                type="text"
-                ref={inputRef}
-                placeholder={vPlaceholder}
-                value={name}
+        <>
+            <input type="text" name="name" value={name}
                 onChange={(e) => setName(e.target.value)}
+                onKeyUp={(e) => handleEnter(e)}
             />
-            <button onClick={handleOnClick} > Add </button>
-            <ul>
-                {
-                    customers.map((customer) => (
-                        <li key={customer.id} >
-                            {customer.name} < button onClick={() => handleDelete(customer.id)}> Delete </button>
-                        </li>
-                    ))}
-            </ul>
-        </div>
-    );
+            <button onClick={handleOnClick}>추가</button>
+        </>
+    )
 };
 
 export default PushAndDelete;
