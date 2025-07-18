49e2808f051202e9a3c2df4327487415f342dda4
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-18 15:12:21 +0900

    Table

--- week.01.v0614/myapp/src/App.js
+++ week.01.v0614/myapp/src/App.js
@@ -6,6 +6,7 @@ import TextArea from './component/TextArea';
 import Select from './component/Select';
 import Booking from './component/Booking';
 import SignUp from './component/SignUp';
+import Table from './chapter_01/Table';
 
 const App = () => {
 
@@ -13,7 +14,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h5>React, Hello World</h5>
-        <SignUp />
+        <Table />
       </header>
     </div>
   );
