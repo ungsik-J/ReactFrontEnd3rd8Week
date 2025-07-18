edb7a464d5d80ed5bfb4a99f7d77905440881299
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-18 10:14:21 +0900

    form SignUp

--- week.01.v0614/myapp/src/App.js
+++ week.01.v0614/myapp/src/App.js
@@ -5,16 +5,15 @@ import Parent from './component/Parent';
 import TextArea from './component/TextArea';
 import Select from './component/Select';
 import Booking from './component/Booking';
+import SignUp from './component/SignUp';
 
 const App = () => {
-
-
 
   return (
     <div className="App">
       <header className="App-header">
         <h5>React, Hello World</h5>
-        <Booking />
+        <SignUp />
       </header>
     </div>
   );
