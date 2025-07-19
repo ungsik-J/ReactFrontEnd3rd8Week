cdc93488aefefc7437d70ce202676158439858fb
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-20 06:16:59 +0900

    src/reducers/dataReducer.js.console.log

--- week.02.v0621/myapp/src/reducers/dataReducer.js
+++ week.02.v0621/myapp/src/reducers/dataReducer.js
@@ -8,14 +8,17 @@ export function dataReducer(state, action) {
     switch (action.type) {
         case 'FETCH_INIT':
             console.log(`reducers\dataReducer.state : ${state}`)
+            console.log(state)
             return { ...state, loading: true, error: null }
             break;
         case 'FETCH_SUCCESS':
             console.log(`reducers\dataReducer.data : ${state}`)
+            console.log(state)
             return { loading: false, data: action.payload, error: null }
             break;
         case 'FETCH_ERROR':
             console.log(`reducers\dataReducer.data : ${state}`)
+            console.log(state)
             return { loading: false, data: [], error: action.error }
             break;
         default:
