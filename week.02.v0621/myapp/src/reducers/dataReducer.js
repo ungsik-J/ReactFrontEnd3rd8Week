8b1d8ebe4288260616a581a87d60c83331d4c195
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-20 06:14:21 +0900

    src/reducers/dataReducer.js.console.log

--- week.02.v0621/myapp/src/reducers/dataReducer.js
+++ week.02.v0621/myapp/src/reducers/dataReducer.js
@@ -7,12 +7,15 @@ export const initialState = {
 export function dataReducer(state, action) {
     switch (action.type) {
         case 'FETCH_INIT':
+            console.log(`reducers\dataReducer.state : ${state}`)
             return { ...state, loading: true, error: null }
             break;
         case 'FETCH_SUCCESS':
+            console.log(`reducers\dataReducer.data : ${data}`)
             return { loading: false, data: action.payload, error: null }
             break;
         case 'FETCH_ERROR':
+            console.log(`reducers\dataReducer.data : ${data}`)
             return { loading: false, data: [], error: action.error }
             break;
         default:
