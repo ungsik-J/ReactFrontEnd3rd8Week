fa27e2d94a0ea2db227b6ad5211fc3ab1af3ced6
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-07-19 11:26:42 +0900

    ProductList.init

--- week.02.v0621/myapp/src/App.jsx
+++ week.02.v0621/myapp/src/App.jsx
@@ -2,20 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 import UseEffect from './component/UseEffect'
+import ProductList from './component/ProductList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  /** UseEffect ****************************************************************
   const [visible, setVisible] = useState(false)
-
+    return (
+      <>
+        <div>
+          <div>
+            <button onClick={() => { setVisible(!visible) }}>
+              {visible ? "hide" : "show"}
+            </button>
+          </div>
+          <div>{visible && <UseEffect />}</div>
+        </div>
+      </>
+    )
+  *************************************************************************** */
   return (
     <>
-      <div>
-        <button onClick={() => { setVisible(!visible) }}>
-          {visible ? "hide" : "show"}
-        </button>
-      </div>
-      {visible && <UseEffect />}
+      <ProductList />
     </>
   )
 }
